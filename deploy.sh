425020f1c644b8f2904501b6e459d5fb2a1e24a9
matesuli <63865991+ciroH@users.noreply.github.com>
2024-05-24 01:53:14 -0300

    Added deploy.sh: deployment script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+APP-DIRECTORY= $PWD;
+
+#---- install nvm (node manager) ----#
+
+echo "INSTALL NVM...";
+cd $HOME;
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh > install-nvm.sh;
+/bin/bash install-nvm.sh;
+echo "...OK";
+
+#---- install node (js engine) ----#
+
+echo "INSTALL NODE...";
+nvm install 20;
+nvm use 20;
+echo "...OK";
+
+#---- install express js (framework) ----#
+
+echo "INSTALL EXPRESS...";
+cd APP-DIRECTORY;
+npm install express;
+echo "...OK";
+
+#---- launching application  ----#
+
+node src/launch/index.js;
